3dc6f720402336ea76cc1b3dfada69bd849d4899
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-06-09 12:38:16 +0530

    fix white issue

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-neutral-50 dark:bg-[#292828] overflow-x-hidden antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-[#292828] overflow-x-hidden antialiased`}>
         <Providers>
           <Appbar />
           {children}
